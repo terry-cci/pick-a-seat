eee8e27a7b4f3773c3f11a76dad90552169f50df
terry-cci <16097988+terry-cci@users.noreply.github.com>
2021-11-17 20:49:52 +0800

    card pile initiation

--- src/components/DeckPile.vue
+++ src/components/DeckPile.vue
@@ -8,12 +8,26 @@ export type StudentCardInfo = {
 
 <script setup lang="ts">
 import DeckSlot from "./DeckSlot.vue";
+import StudentCard from "./StudentCard.vue";
 
-const props = defineProps<{ cards: StudentCardInfo[] }>();
+const props = withDefaults(defineProps<{ cards: StudentCardInfo[] }>(), {
+  cards: () => [],
+});
+
+const CARD_PILING_OFFSET = -0.5;
+const cardStyle = (idx: number) => ({
+  transform: `translateY(${CARD_PILING_OFFSET * idx}px) rotate(-90deg)`,
+});
 </script>
 
 <template>
-  <div>
+  <div class="relative">
     <deck-slot class="w-20 h-32" />
+    <student-card
+      v-for="(card, idx) in cards"
+      :student-info="card.studentInfo"
+      class="absolute w-32 h-20 origin-top-left"
+      :style="cardStyle(idx)"
+    />
   </div>
 </template>
--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -15,4 +15,19 @@ const props = defineProps<{
 }>();
 </script>
 
-<template></template>
+<template>
+  <div
+    class="
+      bg-blue-50
+      rounded
+      shadow-sm
+      border border-gray-600
+      flex flex-col
+      justify-center
+      items-center
+    "
+  >
+    <h2>{{ studentInfo.name.zh }}</h2>
+    <h3 class="text-sm">{{ studentInfo.name.en }} ({{ studentInfo.id }})</h3>
+  </div>
+</template>
